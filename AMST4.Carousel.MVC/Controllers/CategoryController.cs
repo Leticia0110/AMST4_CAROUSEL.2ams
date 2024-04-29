a992a42f46e16cb86659c32fc410b7232644a294
Gabrielbprado <guardavege@gmail.com>
2024-04-29 15:08:29 -0300

    ..

---
+++ AMST4.Carousel.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using AMST4.Carousel.MVC.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMST4.Carousel.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDataContext _applicationDataContext;
+        public CategoryController(ApplicationDataContext applicationDataContext)
+        {
+            _applicationDataContext = applicationDataContext;  
+        }
+        public IActionResult CategoryList()
+        {
+            var categories = _applicationDataContext.Category.ToList();
+
+
+
+            return View(categories);
+        }
+    }
+}
